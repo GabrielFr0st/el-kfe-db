d012555ac7f685ded5a593297b28258e65bdf71b
GabrielFr0st <gabriel.rozas.c@gmail.com>
2025-03-03 00:29:14 -0300

    Corrección de template literals en server.js

--- server.js
+++ server.js
@@ -20,12 +20,11 @@ app.use(cors({
 
 app.use(express.json());
 
-// Rutas
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/blog", blogRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(🚀 Servidor corriendo en puerto ${PORT}));
+app.listen(PORT, () => console.log("🚀 Servidor corriendo en puerto " + PORT));
 
 module.exports = app;
