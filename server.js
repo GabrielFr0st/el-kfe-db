7d1c303659d1abf75f9a073d05da63363e17191b
GabrielFr0st <165584874+GabrielFr0st@users.noreply.github.com>
2025-03-03 00:18:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,24 +7,14 @@ const blogRoutes = require("./routes/blogRoutes");
 
 const app = express();
 
-const allowedOrigins = [
-    "https://el-kfe.netlify.app",
-    "http://localhost:3000" 
-
-app.use(cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
+app.use(cors({ origin: "http://localhost:3000", methods: "GET,POST,PUT,DELETE" }));
 app.use(express.json());
 
-// Rutas
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/blog", blogRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Servidor corriendo en puerto ${PORT}`));
+app.listen(PORT, () => console.log(🚀 Servidor corriendo en puerto ${PORT}));
 
 module.exports = app;
